1f865a97ac2781ead9f1c72618d7d5dbd062f2dc
Uchechuwku Prince Nwulu <uchedotphp@gmail.com>
2024-05-17 15:27:37 +0100

    ADD: Install tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
+    <h1 className="text-3xl text-red-700 font-bold underline">Hello world!</h1>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
