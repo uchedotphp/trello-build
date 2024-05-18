0e8fa4ec3d16ac18d19e3ba60835972eb80005aa
Uchechuwku Prince Nwulu <uchedotphp@gmail.com>
2024-05-18 07:30:27 +0100

    UPDATE: Refactor button to only show if there is content in the parent button tag

---
+++ src/components/base/buttons/BaseBtn.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useSlots, computed } from 'vue'
+const slotsAvailable = useSlots()
+const displayBtn = computed(
+  () => slotsAvailable.leftIcon || slotsAvailable.label || slotsAvailable.rightIcon
+)
+</script>
+
+<template>
+  <button v-if="displayBtn" class="base-btn">
+    <slot v-if="$slots.leftIcon" name="leftIcon" />
+    <span v-if="$slots.label">
+      <slot name="label" />
+    </span>
+    <slot v-if="$slots.rightIcon" name="rightIcon" />
+  </button>
+</template>
+
+<style lang="scss" scoped></style>
