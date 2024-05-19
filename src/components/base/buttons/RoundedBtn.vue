1f7295d1f617c6a5ea249ac06158a1db77450055
Uchechuwku Prince Nwulu <uchedotphp@gmail.com>
2024-05-19 01:42:56 +0100

    ADD: Create a base and rounded button components

---
+++ src/components/base/buttons/RoundedBtn.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { useSlots, computed } from 'vue'
+const slotsAvailable = useSlots()
+const displayBtn = computed(
+  () => slotsAvailable.leftIcon || slotsAvailable.label || slotsAvailable.rightIcon
+)
+</script>
+
+<template>
+  <button v-if="displayBtn" class="base-btn rounded-btn">
+    <slot v-if="$slots.leftIcon" name="leftIcon" />
+    <span v-if="$slots.label">
+      <slot name="label" />
+    </span>
+    <slot v-if="$slots.rightIcon" name="rightIcon" />
+  </button>
+</template>
+
+<style lang="scss" scoped>
+.rounded-btn {
+  @apply p-1.5 rounded-full;
+}
+</style>
