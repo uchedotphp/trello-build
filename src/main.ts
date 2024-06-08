de7e898868e655cae5c129f174000c9f56f95f86
Uchechuwku Prince Nwulu <uchedotphp@gmail.com>
2024-06-09 00:16:38 +0100

    UPDATE

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import './assets/css/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import BaseButton from './components/base/buttons/BaseBtn.vue'
+import RoundedButton from './components/base/buttons/RoundedBtn.vue'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+// Auto import components
+app.component('BaseButton', BaseButton)
+app.component('RoundedButton', RoundedButton)
 
 app.use(createPinia())
 app.use(router)
